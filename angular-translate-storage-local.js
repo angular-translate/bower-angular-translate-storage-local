8997e4895175bba5672826816c1d5e339706d8d9
Francisc Romano <franz.r@gmail.com>
2014-03-15 03:41:50 +0200

    Improved performance by limiting calls to localStorage.getItem().

--- angular-translate-storage-local.js
+++ angular-translate-storage-local.js
@@ -2,14 +2,23 @@ angular.module('pascalprecht.translate').factory('$translateLocalStorage', [
   '$window',
   '$translateCookieStorage',
   function ($window, $translateCookieStorage) {
-    var localStorageAdapter = {
+    var localStorageAdapter = (function() {
+      var langKey;
+      
+      return {
         get: function (name) {
-          return $window.localStorage.getItem(name);
+          if(!langKey) {
+            langKey = $window.localStorage.getItem(name);
+          }
+          
+          return langKey;
         },
         set: function (name, value) {
+          langKey=value;
           $window.localStorage.setItem(name, value);
         }
       };
+    }());
     var $translateLocalStorage = 'localStorage' in $window && $window.localStorage !== null ? localStorageAdapter : $translateCookieStorage;
     return $translateLocalStorage;
   }
