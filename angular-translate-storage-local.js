aeab13d6f3824c4ecf5d3a3bdac74918359c5460
Pascal Precht <pascal.precht@googlemail.com>
2014-06-03 11:51:24 +0200

    updates package

--- angular-translate-storage-local.js
+++ angular-translate-storage-local.js
@@ -1,5 +1,5 @@
 /*!
- * angular-translate - v2.1.0 - 2014-04-02
+ * angular-translate - v2.2.0 - 2014-06-03
  * http://github.com/PascalPrecht/angular-translate
  * Copyright (c) 2014 ; Licensed MIT
  */
@@ -7,15 +7,32 @@ angular.module('pascalprecht.translate').factory('$translateLocalStorage', [
   '$window',
   '$translateCookieStorage',
   function ($window, $translateCookieStorage) {
-    var localStorageAdapter = {
-        get: function (name) {
-          return $window.localStorage.getItem(name);
-        },
-        set: function (name, value) {
-          $window.localStorage.setItem(name, value);
-        }
-      };
-    var $translateLocalStorage = 'localStorage' in $window && $window.localStorage !== null ? localStorageAdapter : $translateCookieStorage;
+    var localStorageAdapter = function () {
+        var langKey;
+        return {
+          get: function (name) {
+            if (!langKey) {
+              langKey = $window.localStorage.getItem(name);
+            }
+            return langKey;
+          },
+          set: function (name, value) {
+            langKey = value;
+            $window.localStorage.setItem(name, value);
+          }
+        };
+      }();
+    var hasLocalStorageSupport = 'localStorage' in $window && $window.localStorage !== null;
+    if (hasLocalStorageSupport) {
+      var testKey = 'pascalprecht.translate.storageTest';
+      try {
+        $window.localStorage.setItem(testKey, 'foo');
+        $window.localStorage.removeItem(testKey);
+      } catch (e) {
+        hasLocalStorageSupport = false;
+      }
+    }
+    var $translateLocalStorage = hasLocalStorageSupport ? localStorageAdapter : $translateCookieStorage;
     return $translateLocalStorage;
   }
 ]);
